9b0976d0aab15d772bb6a1bcea715d30e3d3ae13
Austin <10870864@my.uvu.edu>
2022-11-03 11:51:15 -0600

    made some changes and updates

--- js/main.js
+++ js/main.js
@@ -4,4 +4,4 @@ document.querySelector('header > h2').innerText = "Business Tour Edition"
 
 //giving the body an h1-h2 header.
 document.getElementById('head1').innerHTML = "CMT Engineering Tour";
-document.getElementById('head2').innerHTML = "Take a walk through with this virtual tour of a civil engineering building. ";
+document.getElementById('head2').innerHTML = "Take A Virtual Tour Through In Civil Engineering Building. ";
