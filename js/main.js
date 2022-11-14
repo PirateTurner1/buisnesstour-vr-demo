53dea3f0d4c5aac80aa130cf063df823b458cc32
Austin <10870864@my.uvu.edu>
2022-11-14 13:06:20 -0700

    added a VR object and made other changes to fit it into the site.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //giving the header an h1-h2 title.
 document.querySelector('header > h1').innerText = "Welcome to my World of VR!"
-document.querySelector('header > h2').innerText = "Business Tour Edition"
+document.querySelector('header > h2').innerText = "Where Digital Design Becomes Reality!"
 
 //giving the body an h1-h2 header.
-document.getElementById('head1').innerHTML = "CMT Engineering Tour";
-document.getElementById('head2').innerHTML = "Take A Virtual Tour Through In Civil Engineering Building. ";
+document.getElementById('head1').innerHTML = "Part 1: CMT Engineering Tour";
+document.getElementById('head2').innerHTML = "The CMT Engineering Tour";
